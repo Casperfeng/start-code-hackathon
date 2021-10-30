7b170977a3928771f6a3552208f3fa7e8a91daa9
Isak Collett <isak@Isaks-MacBook-Pro.local>
2021-10-30 18:53:18 +0200

    roomGenerator

---
+++ src/roomGenerator.py
@@ -0,0 +1,19 @@
+import random
+
+list_rooms=["workRoom", "meetRoom", "breakRoom", "kitchen", "bathroom"]
+
+#Create a list of dictionaries
+def createList():
+    numOfRooms=random.randint(1,10)
+    i = 0
+    list_dict=[]
+    for i in range(numOfRooms):
+        roomDict = {
+            "id": i + 1,
+            "width": random.randint(1,6),
+            "height": random.randint(1,4),
+            "type": random.choice(list_rooms)
+        }
+        list_dict.append(roomDict)
+        
+    return list_dict
