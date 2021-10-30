b26cf3e46b294601cc25d63d4bc6dd9a5923ed13
jgpetter <jgpetter@gmail.com>
2021-10-30 18:56:46 +0200

    time to debug

--- python_src/placement.py
+++ python_src/placement.py
@@ -6,12 +6,15 @@ import copy
 
 class RoomPlacer:
 
-    def place(self, filepath):
+    def place(self, filepath=None):
         # Read input problem
-        floor_plan, rooms = self.parse_json(filepath)
+        if filepath:
+            floor_plan, rooms = self.parse_json(filepath)
+        else:
+            self.generate(10)
 
         # Floor width/height
-        self.fp_width, self.fp_height = self.find_floor_dimensions()
+        self.fp_width, self.fp_height = self.find_floor_dimensions(floor_plan)
 
         # Create 2D numpy array 'map' where values are room ids
         floor_map = np.zeros((self.fp_height, self.fp_width))
@@ -24,14 +27,15 @@ class RoomPlacer:
             floor_map, room_type_dict['workRoom'], float('inf'))
 
         # Add the remaining room types in an arbitrary order
+        # TODO: Use a seach tree to find the order that gives the minimum avg. room square height.
         for type in room_type_dict:
             floor_map = self.add_rooms(
                 floor_map, room_type_dict[type], float('inf'))
 
-        # Update the top-left anchor values of rooms
-        self.fill_rooms_values(floor_map,)
+        # TODO: Update the top-left anchor values of rooms and return rooms instead of floor map
+        #       self.fill_rooms_values(floor_map)
 
-        return rooms
+        return floor_map
 
     def parse_json(self, filepath):
         """
@@ -80,35 +84,68 @@ class RoomPlacer:
             else:
                 type_dict[room['type']] += [room]
 
+        # TODO: Sort lists by decreasing size.
+        # TODO: Split lists with length >4 to get smaller clusters.
+
         return type_dict
 
     def add_rooms(self, floor_map, rooms):
-        '''Recursive method that places room of the same type optimally'''
+        '''Places rooms of the same type optimally'''
+
+        # TODO: Currently adds rooms in an arbitrary order.
+        #       Use a seach tree to find the order that gives the minimum avg. room square height.
 
         for room in rooms:
             free_spots = self.find_available_placement(floor_map, room)
 
             min_avg_height = float('inf')
-            best_floor_map = copy.deepcopy(floor_map)
+            cur_best_map = copy.deepcopy(floor_map)
 
             for spot in free_spots:
                 temp_floor_map = copy.deepcopy(floor_map)
 
+                room['anchorTopLeftX'] = spot[0]
+                room['anchorTopLeftY'] = spot[1]
+
+                self.place_room(temp_floor_map, room)
+
                 current_avg_height = self.average_height(
-                    temp_floor_map, rooms['type'])
+                    temp_floor_map, rooms[0]['type'])
 
                 if current_avg_height < min_avg_height:
+                    cur_best_map = temp_floor_map
 
-        return None
+            floor_map = cur_best_map
 
-        # if added_room['type'] == 'workRoom':
-        #     squares = self.find_work_room_squares(cur_floor_map, added_room)
-        # else:
-        #     squares = self.find_room_squares(cur_floor_map, added_room)
+        self.add_path(floor_map, [room['id'] for room in rooms])
 
-        # # Add more checks below that reduces squares
+    def find_available_placement(floor_map, room):
+        '''Finds the top-left anchor points where the room can be placed. A list of anchor points is returned.'''
+        anchor_points = []
 
-    # NOT YET IN USE
+        for y in reversed(range(room['height'])):
+            count = 0  # Count consequtive path squares encountered horizontally
+            for x in range(room['width']):
+                if floor_map[y, x] == -1:
+                    count += 1
+
+                    if count == room['width']:
+                        anchor_points += (x - (room['width'] - 1),
+                                          y - (room['height'] - 1))
+                else:
+                    count = 0
+
+        return anchor_points
+
+    def add_path(self, floor_map, id_list):
+        '''Simple initial path implementation. Adds path (-1) on top of a group of rooms of the same type.'''
+        for y in reversed(range(self.fp_height)):
+            for x in range(self.fp_width):
+                if floor_map[y, x] in id_list:  # Check id_list number format
+                    floor_map[y-1, x] = -1
+                    break
+
+        # TODO: connect paths
 
     def place_room(self, floor_map, room):
         '''Adds the room to the floor map.'''
@@ -118,51 +155,23 @@ class RoomPlacer:
                 floor_map[room['anchorTopLeftY'] + y,
                           room['anchorTopLeftX'] + x] = room['id']
 
-    def find_placement_squares(self, room):
-        '''Finds top-left squares where the given room can be placed'''
-        squares = []  # list of tuples
-
-        # TODO
-
-        return squares
-
     def average_height(self, floor_map, type_int):
         '''Heuristic to compare search solutions'''
         count = 0
         sum_height = 0
 
-        for x in range(room['width']):
-            for y in range(room['height']):
+        for x in range(self.fp_width):
+            for y in range(self.fp_height):
                 if floor_map[y, x] == type_int:
                     count += 1
                     sum_height += y
 
         return sum_height / count
 
-    def fill_rooms_values(floor_map, rooms):
-        rooms_found = []
-
 
 if __name__ == '__main__':
     result = RoomPlacer().place('./TASK_examples/basic_example_input.json')
+
     print('Results of brute-force:\n')
-    pprint.pprint(result)
-
-    # def find_work_room_squares(self, floor_map, room):
-    #     '''Finds possible top-left work_room squares.'''
-    #     for x_val in range(floor_map.shape[1] - room['width']):
-    #         if floor_map[0, x_val] == 0:
-    #             pass
-    #         if floor_map[floor_map.shape[0] - room['height'], x_val] == 0:
-
-    #     for x in range(floor_map.shape[1] - room['width']):
-    #         for y in range(floor_map.shape[0] - room['height']):
-    #             pass
-
-    # def uses_unassigned_squares(floor_map, room):
-    #     for x in range(room['width']):
-    #         for y in range(room['height']):
-    #             if floor_map[room['anchorTopLeftX'] + x, room['anchorTopLeftY'] +y] != 0:
-    #                 return False
-
-    #     return True
+    print('\n'.join([''.join(['{:4}'.format(item) for item in row])
+                     for row in result]))
